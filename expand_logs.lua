c9f4ac86f10dddb70b79554a56a3ac96174d2a06
James DeVries <james.devries@jamcity.com>
2023-03-30 15:28:02 -0500

    Elastic search integration

--- expand_logs.lua
+++ expand_logs.lua
@@ -13,7 +13,14 @@ function cb_split(tag, timestamp, record)
     -- add context to all log entries
     local logtable = {}
     for _, log in pairs(logs) do
-        table.insert(logtable, { log=log, context=context })
+        -- Options for output:
+        -- 1. Make new table and put log and context objects in as keys:
+        -- This requiers the logtable variable, return that at the end of this function
+        -- table.insert(logtable, { log=log, context=context })
+        -- 2. Flat structure: Copy the context keys into the log -
+        -- for k,v in pairs(context) do log[k] = v end
+        -- 3. Context as an object in the log:
+        log[CONTEXT_KEY] = context
     end
-    return 2, timestamp, logtable
+    return 2, timestamp, logs
 end
